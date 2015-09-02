0b134a9d264ffe8e527eef241b25d745b8e1bdd8
schafsberg <stefan_gimborn@web.de>
2015-09-02 20:59:00 +0200

    init

---
+++ SG-contact.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: SG Contact
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A really small Wordpress contact plugin.
+Version: 1.0
+Author: Stefan Gimborn
+Author URI: http://URI_Of_The_Plugin_Author
+License: MIT
+*/
